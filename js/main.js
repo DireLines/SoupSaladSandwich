6ef966a5f845e45579b3e35682fabe602b1c3129
jherd23 <jherd23@gmail.com>
2018-03-24 17:11:37 -0400

    cleaned up some files

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+		$('.button').mouseenter(function(){
+				$(this).fadeTo(50,1);
+		});
+		$('.button').mouseleave(function(){
+				$(this).fadeTo(100,0.7);
+		});
+		$('.year').click(function(){
+
+		});
+});
