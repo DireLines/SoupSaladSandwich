93efa223f1c0dc88bc3ebe50a9cbb82247302054
jherd23 <jherd23@gmail.com>
2018-03-24 12:31:45 -0400

    added some more requests to the server

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
-var express = require('express');
+//Import express -- its what runs the server
 
+var express = require('express');
 var app = express();
 
+
+//Import path so we can serve local files
 var path = require('path');
-
+//Define a certain path.
 var htmldir = __dirname + "/html/";
+var jsdir = __dirname + "/js/";
 
-
+//This handles requests to the main site. If you want to change which file gets served, or make content at runtime, change here
 app.get('/', function(req, res) {
 	res.sendFile(path.join(htmldir + "index.html"));
 });
 
+app.get('/getfoods', function(req, res) {
+	//Connect to database here!
+	res.send('Foods are get.');
+});
+
+app.post('/vote', function(req, res) {
+	res.send('Thanks for voting!');
+});
+
+app.use('/js/', express.static(jsdir));
+
 app.listen(80, function() {
 	console.log("Listening on port 80...");
-})
+});
