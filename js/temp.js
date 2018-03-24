0a6191fc49dac08508df4ddb7310b6970d9964c4
jherd23 <jherd23@gmail.com>
2018-03-24 15:17:00 -0400

    Added a vote function to the front end

--- js/temp.js
+++ js/temp.js
@@ -1,1 +1,31 @@
 //heres some javascript
+
+var FoodType = {
+	SOUP : 0,
+	SALAD : 1,
+	SANDWICH : 2
+}
+
+/**
+ * vote(foodid, type)
+ *
+ * @param foodid: the uniqueid of the food being voted on
+ * @param type: FoodType.SOUP, FoodType.SALAD, or FoodType.SANDWICH
+ *
+ * Sends a vote notification to the website
+ *
+ */
+
+function vote(uniqueid, category) {
+	var request = new XMLHttpRequest();
+	var fd = new FormData();
+	fd.append('uniqueid', foodid);
+	fd.append('category', category);
+	request.onreadystatechange = function() {
+		if(request.readystate == 4 && request.status == 200) {
+			//Handle any feedback from server here.
+		}
+	}
+	request.open('POST', '/vote');
+	request.send(fd);
+}
