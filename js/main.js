0a5c9fb5cd5cd401b482fa48c6191050395993fd
Nathaniel Saxe <NathanielSaxophone@gmail.com>
2018-03-25 08:28:42 -0400

    imgs

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
 $(document).ready(function(){
 	getfoods();
-	var numVoted = 0;
-		$('.button').click(function(){
-			vote(1,0);
-			console.log("Button Press");
-		});
 });
+
+//gets called once list of 5 random foods is recieved from database
+function generateHTML(data) {
+	//set food text
+	parsedFoods = JSON.parse(data);
+	$('div.food h1').text(parsedFoods.response[0].name.toUpperCase());
+	//change image
+	//TODO : replace with google image search
+	$('.food').css('background-image','url("' + parsedFoods.response[0].path + '")');
+	//assign vote functions to buttons
+
+	var numVoted = 0;
+	$('.button').click(function(){
+		console.log("voting on thing!");
+		numVoted++;
+	});
+}
